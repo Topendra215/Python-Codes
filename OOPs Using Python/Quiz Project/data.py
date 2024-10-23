c9986355567d366cb1a7b53a897efc31334c8204
Topendra Bandhan <148429300+Topendra215@users.noreply.github.com>
2024-10-23 22:41:25 +0545

    Quiz Game Project

---
+++ OOPs Using Python/Quiz Project/data.py
@@ -0,0 +1,206 @@
+question_data = [
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "Ada Lovelace is often considered the first computer programmer.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "The Windows 7 operating system has six main editions.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "RAM stands for Random Access Memory.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "&quot;HTML&quot; stands for Hypertext Markup Language.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "Linus Torvalds created Linux and Git.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "Time on Computers is measured via the EPOX System.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Science: Computers",
+                "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Entertainment: Film",
+                "question": "Actor Tommy Chong served prison time.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Entertainment: Film",
+                "question": "In the original script of &quot;The Matrix&quot;, the machines used humans as additional computing power instead of batteries.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Entertainment: Film",
+                "question": "&quot;Minions&quot; was released on the June 10th, 2015.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Entertainment: Film",
+                "question": "Samuel L. Jackson had the words, &#039;Bad Motherf*cker&#039; in-scripted on his lightsaber during the filming of Star Wars.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Entertainment: Film",
+                "question": "In the original Star Wars trilogy, David Prowse was the actor who physically portrayed Darth Vader.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Entertainment: Film",
+                "question": "The word &quot;Inception&quot; came from the 2010 blockbuster hit &quot;Inception&quot;.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Entertainment: Film",
+                "question": "Shaquille O&#039;Neal appeared in the 1997 film &quot;Space Jam&quot;.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Entertainment: Film",
+                "question": "The movie &quot;The Nightmare before Christmas&quot; was all done with physical objects.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Entertainment: Film",
+                "question": "Matt Damon played an astronaut stranded on an extraterrestrial planet in both of the movies Interstellar and The Martian.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "Entertainment: Film",
+                "question": "In Alfred Hitchcock&#039;s film &#039;Psycho&#039; it is said he used chocolate syrup to simulate the blood in the famous shower scene from ",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            }
+            ]
+
+
+
