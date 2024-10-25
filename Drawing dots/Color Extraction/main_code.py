3e61dc5422c57dd100677349cbb72ef88fee48a4
Topendra Bandhan <148429300+Topendra215@users.noreply.github.com>
2024-10-25 20:53:13 +0545

    Extracting Color

---
+++ Drawing dots/Color Extraction/main_code.py
@@ -0,0 +1,16 @@
+import colorgram
+
+color=colorgram.extract('colors.jpeg',30)
+
+rgb_color=[]
+
+for color in color:
+    r=color.rgb.r
+    g=color.rgb.g
+    b=color.rgb.b
+    c_tuple=(r,g,b)
+
+    rgb_color.append(c_tuple)
+
+
+print(rgb_color)
